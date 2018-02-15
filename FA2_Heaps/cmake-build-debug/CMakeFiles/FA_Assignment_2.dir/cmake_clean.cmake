5fa2328d825671caacdb6ae3d694f350d52fce82
Dan Mitrea <dan.mitrea06@gmail.com>
2018-02-15 16:01:50 +0200

    homework 2

---
+++ FA2_Heaps/cmake-build-debug/CMakeFiles/FA_Assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FA_Assignment_2.dir/main.cpp.o"
+  "FA_Assignment_2.pdb"
+  "FA_Assignment_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FA_Assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
