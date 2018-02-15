e6305d11a6837d8951bd16a7218d38a1ea144e59
Dan Mitrea <dan.mitrea06@gmail.com>
2018-02-15 16:05:45 +0200

    homework 4

---
+++ FA4_MergeKSortedArrays/cmake-build-debug/CMakeFiles/FA_assignment4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FA_assignment4.dir/main.cpp.o"
+  "FA_assignment4.pdb"
+  "FA_assignment4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FA_assignment4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
